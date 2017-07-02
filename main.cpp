96cb95b303c1e779dc37a09b00b3146864d61ae4
Vladislav <JestemDevOfficial@gmail.com>
2017-07-02 22:51:55 +0300

    Main file of project

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "Auto_script.h"
+
+using namespace std;
+
+int main() {
+   AutoData Car;
+   Car.set_date();
+   Car.get_date();
+   Car.set_color();
+   Car.get_color();
+   Car.set_wheels();
+   Car.get_wheels();
+   Car.set_doors();
+   Car.get_doors();
+   Car.data();
+
+   cin.get();
+   return 0;
+}
