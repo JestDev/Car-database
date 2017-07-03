07474e0d5fe5e5cff10b72e319ecc654d3474f24
Vladislav <JestemDevOfficial@gmail.com>
2017-07-03 13:33:28 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,8 +11,10 @@ int main() {
    Car.get_date();
    Car.set_color();
    Car.get_color();
-   Car.set_wheels();
-   Car.get_wheels();
+   Car.set_volume();
+   Car.get_volume();
+   Car.set_doors();
+   Car.get_doors();
    Car.data();
 
    cin.get();
